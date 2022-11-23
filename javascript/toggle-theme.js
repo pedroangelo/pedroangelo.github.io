8bb100268e6e1228a2f63d01f7677da011bad319
Pedro Ângelo <pjfangelo@hotmail.com>
2022-11-23 14:57:27 +0000

    take theme preferences into account

--- javascript/toggle-theme.js
+++ javascript/toggle-theme.js
@@ -1,3 +1,25 @@
+/* switch to default theme */
+function switchToDefaultTheme() {
+    /* change local storage entry */
+    localStorage.setItem("current-theme", "default-theme");
+    /* change theme indication */
+    document.getElementById("toggle-theme").innerHTML = "default";
+    /* change body's class */
+    document.body.classList.add("default-theme");
+    document.body.classList.remove("light-theme", "dark-theme");
+}
+
+/* switch to light theme */
+function switchToLightTheme() {
+    /* change local storage entry */
+    localStorage.setItem("current-theme", "light-theme");
+    /* change theme indication */
+    document.getElementById("toggle-theme").innerHTML = "light";
+    /* change body's class */
+    document.body.classList.add("light-theme");
+    document.body.classList.remove("default-theme", "dark-theme");
+}
+
 /* switch to dark theme */
 function switchToDarkTheme() {
     /* change local storage entry */
@@ -6,27 +28,20 @@ function switchToDarkTheme() {
     document.getElementById("toggle-theme").innerHTML = "dark";
     /* change body's class */
     document.body.classList.add("dark-theme");
-    document.body.classList.remove("light-theme");
-}
-
-function switchToLightTheme() {
-    /* change local storage entry */
-    localStorage.setItem("current-theme", "light-theme");
-    /* change theme indication */
-    document.getElementById("toggle-theme").innerHTML = "light";
-    /* change body's class */
-    document.body.classList.add("light-theme");
-    document.body.classList.remove("dark-theme");
+    document.body.classList.remove("default-theme", "light-theme");
 }
 
 /* toggle between themes */
 function toggleTheme() {
     switch (localStorage.getItem("current-theme")) {
+    case "default-theme" :
+	switchToLightTheme();
+	break;
     case "light-theme" :
 	switchToDarkTheme();
 	break;
     case "dark-theme" :
-	switchToLightTheme();
+	switchToDefaultTheme();
 	break;
     }
 }
@@ -35,9 +50,12 @@ function toggleTheme() {
 function initializeTheme() {
     /* check local storage for theme preferences */
     if (localStorage.getItem("current-theme") === null) {
-	switchToLightTheme();
+	switchToDefaultTheme();
     } else {
 	switch (localStorage.getItem("current-theme")) {
+	case "default-theme" :
+	    switchToDefaultTheme();
+	    break;
 	case "light-theme" :
 	    switchToLightTheme();
 	    break;
