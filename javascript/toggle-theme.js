aaa62048e2789fb0706f35ee3726f5c796c3d482
Pedro Ângelo <pjfangelo@hotmail.com>
2022-11-27 21:08:24 +0000

    add dropdown theme picker

--- javascript/toggle-theme.js
+++ javascript/toggle-theme.js
@@ -1,47 +1,34 @@
-/* switch to default theme */
-function switchToDefaultTheme() {
-    /* change local storage entry */
-    localStorage.setItem("current-theme", "default-theme");
-    /* change theme indication */
-    document.getElementById("toggle-theme").innerHTML = "default";
-    /* change body's class */
-    document.body.classList.add("default-theme");
-    document.body.classList.remove("light-theme", "dark-theme");
-}
+/* define all possible themes */
+let themeList = ["default", "light", "dark"];
 
-/* switch to light theme */
-function switchToLightTheme() {
+/* switch to theme */
+function switchToTheme(theme) {
     /* change local storage entry */
-    localStorage.setItem("current-theme", "light-theme");
-    /* change theme indication */
-    document.getElementById("toggle-theme").innerHTML = "light";
+    localStorage.setItem("current-theme", theme + "-theme");
+    /* get theme-picker div element */
+    let themePicker = document.querySelector("div#theme-picker");
+    /* get toggle-theme button element */
+    let toggleThemeButton = themePicker.querySelector("button.toggle-theme");
+    /* get theme-options div element */
+    let themeOptions = themePicker.querySelector("div.theme-options");
     /* change body's class */
-    document.body.classList.add("light-theme");
-    document.body.classList.remove("default-theme", "dark-theme");
-}
-
-/* switch to dark theme */
-function switchToDarkTheme() {
-    /* change local storage entry */
-    localStorage.setItem("current-theme", "dark-theme");
-    /* change theme indication */
-    document.getElementById("toggle-theme").innerHTML = "dark";
-    /* change body's class */
-    document.body.classList.add("dark-theme");
-    document.body.classList.remove("default-theme", "light-theme");
+    for (let i = 0; i < themeList.length; i++) {
+	document.body.classList.remove(themeList[i] + "-theme");
+    }
+    document.body.classList.add(theme + "-theme");
 }
 
 /* toggle between themes */
 function toggleTheme() {
     switch (localStorage.getItem("current-theme")) {
     case "default-theme" :
-	switchToLightTheme();
+	switchToTheme("light");
 	break;
     case "light-theme" :
-	switchToDarkTheme();
+	switchToTheme("dark");
 	break;
     case "dark-theme" :
-	switchToDefaultTheme();
+	switchToTheme("default");
 	break;
     }
 }
@@ -50,18 +37,37 @@ function toggleTheme() {
 function initializeTheme() {
     /* check local storage for theme preferences */
     if (localStorage.getItem("current-theme") === null) {
-	switchToDefaultTheme();
+	switchToTheme("default");
     } else {
 	switch (localStorage.getItem("current-theme")) {
 	case "default-theme" :
-	    switchToDefaultTheme();
+	    switchToTheme("default");
 	    break;
 	case "light-theme" :
-	    switchToLightTheme();
+	    switchToTheme("light");
 	    break;
 	case "dark-theme" :
-	    switchToDarkTheme();
+	    switchToTheme("dark");
 	    break;
 	}
     }
+}
+
+
+/* NOT USED */
+
+/* add theme-options buttons */
+function addThemeOptionsButtons() {
+    /* get theme-picker div element */
+    let themePicker = document.querySelector("div#theme-picker");
+    /* get theme-options div element */
+    let themeOptions = themePicker.querySelector("div.theme-options");
+    /* change theme options */
+    let themeOptionsHTML = "";
+    /* add HTML code for a button for each theme */
+    for (let i = 0; i < themeList.length; i++) {
+	    themeOptionsHTML = themeOptionsHTML + "<button class=\"pick-theme\" onclick=\"switchToTheme(\'" + themeList[i] + "\')\">" + themeList[i] + "</button>";
+    }
+    /* add code to theme-options */
+    themeOptions.innerHTML = themeOptionsHTML;
 }
