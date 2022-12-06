202e8831b05e71af40355da457eeecabae0ee331
Pedro Ângelo <pjfangelo@hotmail.com>
2022-12-06 12:42:23 +0000

    correct comments in javascript files

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,80 +1,80 @@
-/* get paper references */
+// get paper references
 function getPaperReferences() {
-    /* get all .paper-entry elements */
+    // get all .paper-entry elements
     let paperEntries = document.querySelectorAll(".paper-entry");
-    /* initialize references variables */
+    // initialize references variables
     let paperReferences = [];
-    /* for each .paper-entry, collect its paper reference */
+    // for each .paper-entry, collect its paper reference
     for (let i = 0; i < paperEntries.length; i++) {
 	paperReferences.push(paperEntries[i].classList[1]);
     }
     return paperReferences;
 }
 
-/* get modal element from paper reference */
+// get modal element from paper reference
 function getModal(paperReference) {
     return document.querySelector("." + paperReference + " div.modal");
 }
 
-/* get modal element from child object */
+// get modal element from child object
 function getModalFromChild(object) {
     let paperReference = object.closest(".paper-entry").classList[1];
     return getModal(paperReference);
 }
 
-/* fill modal with information */
+// fill modal with information
 function populateModal(paperReference) {
-    /* get selected modal */
+    // get selected modal
     let modal = getModal(paperReference);
-    /* get modal text element */
+    // get modal text element
     let modalText = modal.querySelector("div.modal-content p");
-    /* get bib file contents */
+    // get bib file contents
     let content = paperReference;
     modalText.innerHTML = content;
 }
 
-/* initialize modals */
+// initialize modals
 function initializeModals() {
-    /* get paper references */
+    // get paper references
     let paperReferences = getPaperReferences();
-    /* for each paper reference: */
+    // for each paper reference:
     for (let i = 0; i < paperReferences.length; i++) {
-	/* populate modal with information relative to paper reference */
+	// populate modal with information relative to paper reference
 	populateModal(paperReferences[i]);
-	/* add event listener to close modal on click */
+	// add event listener to close modal on click
 	window.addEventListener('click', function(event) { if (event.target == getModal(paperReferences[i])) { hideModal(paperReferences[i]); }});
     }
 }
 
-/* show selected modal */
+// show selected modal
 function showModal(reference) {
     let modal = getModal(reference);
     modal.style.display = "block";
 }
 
-/* hide selected modal */
+// hide selected modal
 function hideModal(reference) {
     let modal = getModal(reference);
     modal.style.display = "none";
 }
 
-/* show modal from child object */
+// show modal from child object
 function showModalFromChild(object) {
     let modal = getModalFromChild(object);
     modal.style.display = "block";    
 }
 
-/* hide modal from child object */
+// hide modal from child object
 function hideModalFromChild(object) {
     let modal = getModalFromChild(object);
     modal.style.display = "none";    
 }
 
-/* hide all modal */
+// hide all modal
 function hideAllModal() {
-    /* get selected modal */
+    // get selected modal
     let modal = document.querySelector("div.modal");
-    /* hide modal by changing css style */
+    // hide modal by changing css style
     for (let i = 0; i < modal.length; i++) {
 	modal[i].style.display = "none";
     }
--- javascript/theme-picker.js
+++ javascript/theme-picker.js
@@ -1,24 +1,24 @@
-/* define all possible themes */
+// define all possible themes
 let themeList = ["default", "light", "dark"];
 
-/* switch to theme */
+// switch to theme
 function switchToTheme(theme) {
-    /* change local storage entry */
+    // change local storage entry
     localStorage.setItem("current-theme", theme + "-theme");
-    /* get theme-picker div element */
+    // get theme-picker div element
     let themePicker = document.querySelector("div#theme-picker");
-    /* get toggle-theme button element */
+    // get toggle-theme button element
     let toggleThemeButton = themePicker.querySelector("button.toggle-theme");
-    /* get theme-options div element */
+    // get theme-options div element
     let themeOptions = themePicker.querySelector("div.theme-options");
-    /* change body's class */
+    // change body's class
     for (let i = 0; i < themeList.length; i++) {
 	document.body.classList.remove(themeList[i] + "-theme");
     }
     document.body.classList.add(theme + "-theme");
 }
 
-/* toggle between themes */
+// toggle between themes
 function toggleTheme() {
     switch (localStorage.getItem("current-theme")) {
     case "default-theme" :
@@ -33,9 +33,9 @@ function toggleTheme() {
     }
 }
 
-/* initialize theme by remembering preferences */
+// initialize theme by remembering preferences
 function initializeTheme() {
-    /* check local storage for theme preferences */
+    // check local storage for theme preferences
     if (localStorage.getItem("current-theme") === null) {
 	switchToTheme("default");
     } else {
@@ -54,20 +54,20 @@ function initializeTheme() {
 }
 
 
-/* NOT USED */
+// NOT USED
 
-/* add theme-options buttons */
+// add theme-options buttons
 function addThemeOptionsButtons() {
-    /* get theme-picker div element */
+    // get theme-picker div element
     let themePicker = document.querySelector("div#theme-picker");
-    /* get theme-options div element */
+    // get theme-options div element
     let themeOptions = themePicker.querySelector("div.theme-options");
-    /* change theme options */
+    // change theme options
     let themeOptionsHTML = "";
-    /* add HTML code for a button for each theme */
+    // add HTML code for a button for each theme
     for (let i = 0; i < themeList.length; i++) {
 	    themeOptionsHTML = themeOptionsHTML + "<button class=\"pick-theme\" onclick=\"switchToTheme(\'" + themeList[i] + "\')\">" + themeList[i] + "</button>";
     }
-    /* add code to theme-options */
+    // add code to theme-options
     themeOptions.innerHTML = themeOptionsHTML;
 }
