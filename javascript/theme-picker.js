f7673f5ab691ad279b4bcde7b83c100def2d33c5
Pedro Ângelo <pjfangelo@hotmail.com>
2022-12-07 19:39:47 +0000

    add icons to theme picker, change header behaviour

--- javascript/theme-picker.js
+++ javascript/theme-picker.js
@@ -1,21 +1,48 @@
 // define all possible themes
 let themeList = ["default", "light", "dark"];
 
+// get icon code corresponding to theme
+function getIconCode(theme) {
+    switch (theme) {
+    case "default" :
+	return "brightness_6";
+	break;
+    case "light" :
+	return "brightness_high";
+	break;
+    case "dark" :
+	return "brightness_4";
+	break;
+    }
+}
+
 // switch to theme
 function switchToTheme(theme) {
+    // changes in body
+    
     // change local storage entry
     localStorage.setItem("current-theme", theme + "-theme");
-    // get theme-picker div element
-    let themePicker = document.querySelector("div#theme-picker");
-    // get toggle-theme button element
-    let toggleThemeButton = themePicker.querySelector("button.toggle-theme");
-    // get theme-options div element
-    let themeOptions = themePicker.querySelector("div.theme-options");
     // change body's class
     for (let i = 0; i < themeList.length; i++) {
 	document.body.classList.remove(themeList[i] + "-theme");
     }
     document.body.classList.add(theme + "-theme");
+
+    // changes in theme picker
+    
+    // get theme-picker div element
+    let themePicker = document.querySelector("div#theme-picker");
+    // get theme icon element inside theme button
+    let themeButtonIcon = themePicker.querySelector("button.theme-button span.theme-icon");
+    // change theme button icon
+    themeButtonIcon.innerHTML = getIconCode(theme);
+    // remove class active from all pic-theme button
+    let pickThemeButtons = themePicker.querySelectorAll("div.theme-options button.pick-theme");
+    for (let i = 0; i < pickThemeButtons.length; i++) {
+	pickThemeButtons[i].classList.remove("active");
+    }
+    // add class active to pick-theme button
+    themePicker.querySelector("div.theme-options button.pick-" + theme).classList.add("active");
 }
 
 // toggle between themes
